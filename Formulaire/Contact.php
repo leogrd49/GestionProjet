cfbd6e606de521c62834148c8ae1cf4705d952c3
Parizetm <151616242+ParizetM@users.noreply.github.com>
2024-04-24 11:50:28 +0200

    Create Contact.php

---
+++ Formulaire/Contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact.css">
+    <title>Contactez nous !</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <form action="">
+            <input type="text" name>
+        </form>
+    </main>
+    <footer></footer>
+</body>
+</html>
